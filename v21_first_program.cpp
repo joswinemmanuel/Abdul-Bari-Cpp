5340e403e35141b1b0bd1ddc49cd0b853f4f5093
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-12 06:53:26 +0530

    Add files via upload

---
+++ v21_first_program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout << "Enter the first number : ";
+    cin >> a;
+    cout << "Enter the second number : ";
+    cin >> b;
+    c = a + b;
+    cout << "Sum is : " << c << endl;
+    return 0;
+}
