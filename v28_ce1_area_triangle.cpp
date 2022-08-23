44ec22469d6ef6fe167a0d82d4c7ee81f0b19da4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-23 09:54:01 +0530

    Add files via upload

---
+++ v28_ce1_area_triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void area() 
+{
+    int b=7,h=5;
+   
+    float area;
+	
+    area=(1.0/2) * b * h;
+    
+ 
+ cout<<area;
+}
+
+int main(){
+    area();
+}
