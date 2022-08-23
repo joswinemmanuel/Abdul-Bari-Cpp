cb85f267e011b690e7c2fec3fc1ae17d520e677e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-23 11:36:25 +0530

    Add files via upload

---
+++ v29_ce2_sum_of_n.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int SumOfN(int n)
+{
+    int sum;
+    
+    //Write formula here as sum=
+    sum= (n * (n+1))/2;
+    
+    return sum;
+}
+
+int main(){
+    printf("%d", SumOfN(5));
+}
